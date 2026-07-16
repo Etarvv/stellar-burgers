324bef6d7639cde7eacf7d524744a7669439fb63
Leonid Tkachenko <etarvv@yandex.ru>
2026-07-16 23:24:48 +0300

    1

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="node" />
-
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
@@ -35,5 +33,8 @@ export default defineConfig({
     url: 'http://localhost:4000',
     reuseExistingServer: !process.env.CI,
     timeout: 180 * 1000,
+    env: {
+      BURGER_API_URL: 'https://norma.education-services.ru', // <-- добавить
+    },
   },
 });
